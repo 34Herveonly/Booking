724c6cc7b9123fa7e1c1a7f3f934267517ae585f
onlyherve34 <herveonly34@gmail.com>
2025-04-14 17:19:50 +0200

    initial commit

---
+++ src/main/java/com/example/Booking/Model/Routes.java
@@ -0,0 +1,67 @@
+package com.example.Booking.Model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "user_input_routes")
+public class Routes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String departure_province;
+    private String departure_district;
+    private String destination_province;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDeparture_province() {
+        return departure_province;
+    }
+
+    public void setDeparture_province(String departure_province) {
+        this.departure_province = departure_province;
+    }
+
+    public String getDeparture_district() {
+        return departure_district;
+    }
+
+    public void setDeparture_district(String departure_district) {
+        this.departure_district = departure_district;
+    }
+
+    public String getDestination_province() {
+        return destination_province;
+    }
+
+    public void setDestination_province(String destination_province) {
+        this.destination_province = destination_province;
+    }
+
+    public String getDestination_district() {
+        return destination_district;
+    }
+
+    public void setDestination_district(String destination_district) {
+        this.destination_district = destination_district;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    private String destination_district;
+    private BigDecimal price;
+}
