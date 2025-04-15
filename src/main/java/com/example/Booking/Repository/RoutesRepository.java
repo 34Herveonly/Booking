66ec24103742da01372e89bcc8c3a1a390000f44
onlyherve34 <herveonly34@gmail.com>
2025-04-15 17:01:07 +0200

    created a service and repository classes  for our system

---
+++ src/main/java/com/example/Booking/Repository/RoutesRepository.java
@@ -0,0 +1,10 @@
+package com.example.Booking.Repository;
+
+import com.example.Booking.Model.Routes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutesRepository extends JpaRepository<Routes, Long> {
+
+}
