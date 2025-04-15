3d99c193cf35058f72ff4af4de7df830229fbd95
onlyherve34 <herveonly34@gmail.com>
2025-04-15 17:18:33 +0200

    added saving logic to the service class

--- src/main/java/com/example/Booking/Service/RoutesService.java
+++ src/main/java/com/example/Booking/Service/RoutesService.java
@@ -1,8 +1,19 @@
 package com.example.Booking.Service;
 
+import com.example.Booking.Model.Routes;
+import com.example.Booking.Repository.RoutesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class RoutesService {
 
+    @Autowired
+    private RoutesRepository RoutesRepository;
+
+
+    public Routes saveUserSelectedRoutes(Routes routes) {
+        return RoutesRepository.save(routes);
+    }
 }
